f91b22e83e5f057bd0ec42ff8f5384789661d1f6
Nikhil Yadav <nikhilyadav.prof@gmail.com>
2026-06-28 16:04:42 +0530

    learn: Hydartion and Hydreation error

---
+++ Section3/Lecture22-23/hydration-hydration-error/app/blogs/page.js
@@ -0,0 +1,40 @@
+import Likes from "@/components/Likes";
+import Link from "next/link";
+
+const Blogs = () => {
+  console.log("Blogs Page");
+  return (
+    <>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link href="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/blogs" className="nav-link active">
+              Blogs
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>Welcome to Our Blog</h1>
+        <Likes />
+      </div>
+    </>
+  );
+};
+
+export default Blogs;
---
+++ Section3/Lecture22-23/hydration-hydration-error/components/Likes.js
@@ -0,0 +1,6 @@
+export default function Likes() {
+  if (typeof window === "undefined") {
+    return <div>sdbfekjb,dskvdsbsbduvdskscjdshvdc</div>;
+  }
+  return <div>Likes 2k</div>;
+}
